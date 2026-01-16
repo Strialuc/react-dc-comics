72720d8f0e6d24efc29122083b939abaef3e5117
Luca Striano <luca.striano90@gmail.com>
2026-01-16 16:24:52 +0100

    creazione component jsx per header e main

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import MainHeader from './components/MainHeader'
+import MainContent from './components/MainContent'
 
 function App() {
 
 
   return (
     <>
-
+      <MainHeader />
+      <MainContent />
     </>
   )
 }
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,7 @@
+const MainContent = () => {
+    return (
+        <h1>Questo è il mio Main</h1>
+    )
+}
+
+export default MainContent
